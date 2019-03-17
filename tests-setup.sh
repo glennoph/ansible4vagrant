825c6e5739625349603d8b03af267e6a884c05e0
glenn <glennoph@gmail.com>
2019-03-17 10:09:48 -0500

    add workaround to tests-setup.sh

--- tests-setup.sh
+++ tests-setup.sh
@@ -1,5 +1,7 @@
 echo 'pip install testinfra paramiko'
 pip install testinfra paramiko
+echo 'workaround for msg CryptographyDeprecationWarning: encode_point has been deprecated on EllipticCurvePublicNumbers'
+pip install cryptography==2.4.2
 
 #echo 'save ssh config'
 #vagrant ssh-config > .vagrant/ssh-config
